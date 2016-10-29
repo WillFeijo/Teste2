19279a89aa1ee7de3b0080de6619db91ec9b793e
WillFeijo <wiliamfeijo@gmail.com>
2016-10-29 09:31:14 -0200

    Add project files.

---
+++ Teste2/MainWindow.xaml.cs
@@ -0,0 +1,324 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Runtime.InteropServices.WindowsRuntime;
+//using Windows.Foundation;
+//using Windows.Foundation.Collections;
+//using Windows.UI.Xaml;
+//using Windows.UI.Xaml.Controls;
+//using Windows.UI.Xaml.Controls.Primitives;
+//using Windows.UI.Xaml.Data;
+//using Windows.UI.Xaml.Input;
+//using Windows.UI.Xaml.Media;
+//using Windows.UI.Xaml.Navigation;
+
+//Library
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
+using Windows.Media.SpeechRecognition;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Media;
+using Windows.Globalization;
+using Windows.UI.Xaml.Documents;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace Teste2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public sealed partial class MainWindow : System.Windows.Window
+    {
+        // Reference to the main sample page in order to post status messages.
+        //private MainWindow rootPage;
+
+        // Speech events may come in on a thread other than the UI thread, keep track of the UI thread's
+        // dispatcher, so we can update the UI in a thread-safe manner.
+        //public CoreDispatcher dispatcher;
+
+        // The speech recognizer used throughout this sample.
+        private SpeechRecognizer speechRecognizer;
+
+        // Keep track of whether the continuous recognizer is currently running, so it can be cleaned up appropriately.
+        private bool isListening;
+
+        // Keep track of existing text that we've accepted in ContinuousRecognitionSession_ResultGenerated(), so
+        // that we can combine it and Hypothesized results to show in-progress dictation mid-sentence.
+        private StringBuilder dictatedTextBuilder;
+
+        /// <summary>
+        /// This HResult represents the scenario where a user is prompted to allow in-app speech, but 
+        /// declines. This should only happen on a Phone device, where speech is enabled for the entire device,
+        /// not per-app.
+        /// </summary>
+        private static uint HResultPrivacyStatementDeclined = 0x80045509;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            isListening = false;
+            dictatedTextBuilder = new StringBuilder();
+        }
+
+        private async void Window_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //rootPage = this;
+
+            // Keep track of the UI thread dispatcher, as speech events will come in on a separate thread.
+            //var dispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
+
+            // Prompt the user for permission to access the microphone. This request will only happen
+            // once, it will not re-prompt if the user rejects the permission.
+            bool permissionGained = await Classes.Audio_Permissions.RequestMicrophonePermission();
+            if (permissionGained)
+            {
+                btnContinuousRecognize.IsEnabled = true;
+
+                await InitializeRecognizer(SpeechRecognizer.SystemSpeechLanguage);
+            }
+            else
+            {
+                DictationTextBox.Text = "Permission to access capture resources was not given by the user, reset the application setting in Settings->Privacy->Microphone.";
+                btnContinuousRecognize.IsEnabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Initialize Speech Recognizer and compile constraints.
+        /// </summary>
+        /// <param name="recognizerLanguage">Language to use for the speech recognizer</param>
+        /// <returns>Awaitable task.</returns>
+        private async Task InitializeRecognizer(Language recognizerLanguage)
+        {
+            if (speechRecognizer != null)
+            {
+                // cleanup prior to re-initializing this scenario.
+                speechRecognizer.StateChanged -= SpeechRecognizer_StateChanged;
+                speechRecognizer.ContinuousRecognitionSession.Completed -= ContinuousRecognitionSession_Completed;
+                speechRecognizer.ContinuousRecognitionSession.ResultGenerated -= ContinuousRecognitionSession_ResultGenerated;
+                speechRecognizer.HypothesisGenerated -= SpeechRecognizer_HypothesisGenerated;
+
+                speechRecognizer.Dispose();
+                speechRecognizer = null;
+            }
+
+            speechRecognizer = new SpeechRecognizer(recognizerLanguage);
+
+            // Provide feedback to the user about the state of the recognizer. This can be used to provide visual feedback in the form
+            // of an audio indicator to help the user understand whether they're being heard.
+            speechRecognizer.StateChanged += SpeechRecognizer_StateChanged;
+
+            // Apply the dictation topic constraint to optimize for dictated freeform speech.
+            var dictationConstraint = new SpeechRecognitionTopicConstraint(SpeechRecognitionScenario.Dictation, "dictation");
+            speechRecognizer.Constraints.Add(dictationConstraint);
+            SpeechRecognitionCompilationResult result = await speechRecognizer.CompileConstraintsAsync();
+            if (result.Status != SpeechRecognitionResultStatus.Success)
+            {
+                DictationTextBox.Text = "Grammar Compilation Failed: " + result.Status.ToString();
+                btnContinuousRecognize.IsEnabled = false;
+            }
+
+            // Handle continuous recognition events. Completed fires when various error states occur. ResultGenerated fires when
+            // some recognized phrases occur, or the garbage rule is hit. HypothesisGenerated fires during recognition, and
+            // allows us to provide incremental feedback based on what the user's currently saying.
+            speechRecognizer.ContinuousRecognitionSession.Completed += ContinuousRecognitionSession_Completed;
+            speechRecognizer.ContinuousRecognitionSession.ResultGenerated += ContinuousRecognitionSession_ResultGenerated;
+            speechRecognizer.HypothesisGenerated += SpeechRecognizer_HypothesisGenerated;
+        }
+
+        private async void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (speechRecognizer != null)
+            {
+                if (isListening)
+                {
+                    await speechRecognizer.ContinuousRecognitionSession.CancelAsync();
+                    isListening = false;
+                    btnContinuousRecognize.Content = "Speak";
+                }
+
+                DictationTextBox.Text = "";
+
+                speechRecognizer.ContinuousRecognitionSession.Completed -= ContinuousRecognitionSession_Completed;
+                speechRecognizer.ContinuousRecognitionSession.ResultGenerated -= ContinuousRecognitionSession_ResultGenerated;
+                speechRecognizer.HypothesisGenerated -= SpeechRecognizer_HypothesisGenerated;
+                speechRecognizer.StateChanged -= SpeechRecognizer_StateChanged;
+
+                speechRecognizer.Dispose();
+                speechRecognizer = null;
+            }
+        }
+
+        /// <summary>
+        /// Handle events fired when error conditions occur, such as the microphone becoming unavailable, or if
+        /// some transient issues occur.
+        /// </summary>
+        /// <param name="sender">The continuous recognition session</param>
+        /// <param name="args">The state of the recognizer</param>
+        private async void ContinuousRecognitionSession_Completed(SpeechContinuousRecognitionSession sender, SpeechContinuousRecognitionCompletedEventArgs args)
+        {
+            if (args.Status != SpeechRecognitionResultStatus.Success)
+            {
+                // If TimeoutExceeded occurs, the user has been silent for too long. We can use this to 
+                // cancel recognition if the user in dictation mode and walks away from their device, etc.
+                // In a global-command type scenario, this timeout won't apply automatically.
+                // With dictation (no grammar in place) modes, the default timeout is 20 seconds.
+                if (args.Status == SpeechRecognitionResultStatus.TimeoutExceeded)
+                {
+                    await Dispatcher.InvokeAsync(() => {
+                        btnContinuousRecognize.Content = "Speak";
+                        DictationTextBox.Text = dictatedTextBuilder.ToString() + "Automatic Time Out of Dictation";
+                        isListening = false;
+                    });
+                }
+                else
+                {
+                    await Dispatcher.InvokeAsync(() => {
+                        DictationTextBox.Text = "Continuous Recognition Completed: " + args.Status.ToString();
+                        btnContinuousRecognize.Content = "Speak";
+                        isListening = false;
+                    });
+                }
+            }
+        }
+
+        /// <summary>
+        /// While the user is speaking, update the textbox with the partial sentence of what's being said for user feedback.
+        /// </summary>
+        /// <param name="sender">The recognizer that has generated the hypothesis</param>
+        /// <param name="args">The hypothesis formed</param>
+        private async void SpeechRecognizer_HypothesisGenerated(SpeechRecognizer sender, SpeechRecognitionHypothesisGeneratedEventArgs args)
+        {
+            string hypothesis = args.Hypothesis.Text;
+
+            // Update the textbox with the currently confirmed text, and the hypothesis combined.
+            string textboxContent = dictatedTextBuilder.ToString() + " " + hypothesis + " ...";
+            await Dispatcher.InvokeAsync(() => {
+                DictationTextBox.Text = textboxContent;
+            });
+        }
+
+        /// <summary>
+        /// Handle events fired when a result is generated. Check for high to medium confidence, and then append the
+        /// string to the end of the stringbuffer, and replace the content of the textbox with the string buffer, to
+        /// remove any hypothesis text that may be present.
+        /// </summary>
+        /// <param name="sender">The Recognition session that generated this result</param>
+        /// <param name="args">Details about the recognized speech</param>
+        private async void ContinuousRecognitionSession_ResultGenerated(SpeechContinuousRecognitionSession sender, SpeechContinuousRecognitionResultGeneratedEventArgs args)
+        {
+            // We may choose to discard content that has low confidence, as that could indicate that we're picking up
+            // noise via the microphone, or someone could be talking out of earshot.
+            if (args.Result.Confidence == SpeechRecognitionConfidence.Medium ||
+                args.Result.Confidence == SpeechRecognitionConfidence.High)
+            {
+                dictatedTextBuilder.Append(args.Result.Text + " ");
+
+                await Dispatcher.InvokeAsync(() => {
+                          DictationTextBox.Text = dictatedTextBuilder.ToString();
+                      });
+            }
+            else
+            {
+                // In some scenarios, a developer may choose to ignore giving the user feedback in this case, if speech
+                // is not the primary input mechanism for the application.
+                // Here, just remove any hypothesis text by resetting it to the last known good.
+                await Dispatcher.InvokeAsync(() => {
+                    DictationTextBox.Text = dictatedTextBuilder.ToString();
+                    string discardedText = args.Result.Text;
+                    if (!string.IsNullOrEmpty(discardedText))
+                    {
+                        discardedText = discardedText.Length <= 25 ? discardedText : (discardedText.Substring(0, 25) + "...");
+                        DictationTextBox.Text = "Discarded due to low/rejected Confidence: " + discardedText;
+                    }
+                });
+            }
+        }
+
+        /// <summary>
+        /// Provide feedback to the user based on whether the recognizer is receiving their voice input.
+        /// </summary>
+        /// <param name="sender">The recognizer that is currently running.</param>
+        /// <param name="args">The current state of the recognizer.</param>
+        private async void SpeechRecognizer_StateChanged(SpeechRecognizer sender, SpeechRecognizerStateChangedEventArgs args)
+        {
+            await Dispatcher.InvokeAsync(() => {
+                DictationTextBox.Text = args.State.ToString();
+            });
+        }
+
+
+        /// <summary>
+        /// Begin recognition, or finish the recognition session. 
+        /// </summary>
+        /// <param name="sender">The button that generated this event</param>
+        /// <param name="e">Unused event details</param>
+        private async void ContinuousRecognize_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            btnContinuousRecognize.IsEnabled = false;
+            if (isListening == false)
+            {
+                // The recognizer can only start listening in a continuous fashion if the recognizer is currently idle.
+                // This prevents an exception from occurring.
+                if (speechRecognizer.State == SpeechRecognizerState.Idle)
+                {
+                    btnContinuousRecognize.Content = "Stop";
+
+                    try
+                    {
+                        isListening = true;
+                        await speechRecognizer.ContinuousRecognitionSession.StartAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        if ((uint)ex.HResult == HResultPrivacyStatementDeclined)
+                        {
+                            // Show a UI link to the privacy settings.
+                            //hlOpenPrivacySettings.Visibility = Visibility.Visible;
+                        }
+                        else
+                        {
+                            var messageDialog = new Windows.UI.Popups.MessageDialog(ex.Message, "Exception");
+                            await messageDialog.ShowAsync();
+                        }
+
+                        isListening = false;
+                        btnContinuousRecognize.Content = "Speak";
+                    }
+                }
+            }
+            else
+            {
+                isListening = false;
+                btnContinuousRecognize.Content = "Speak";
+
+                if (speechRecognizer.State != SpeechRecognizerState.Idle)
+                {
+                    // Cancelling recognition prevents any currently recognized speech from
+                    // generating a ResultGenerated event. StopAsync() will allow the final session to 
+                    // complete.
+                    try
+                    {
+                        await speechRecognizer.ContinuousRecognitionSession.StopAsync();
+
+                        // Ensure we don't leave any hypothesis text behind
+                        DictationTextBox.Text = dictatedTextBuilder.ToString();
+                    }
+                    catch (Exception exception)
+                    {
+                        var messageDialog = new Windows.UI.Popups.MessageDialog(exception.Message, "Exception");
+                        await messageDialog.ShowAsync();
+                    }
+                }
+            }
+            btnContinuousRecognize.IsEnabled = true;
+        }
+    }
+}
